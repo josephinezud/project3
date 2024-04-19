025889a69f17f26f0d8a87153dfb1cf139fffb55
Josephine Zundel <josephine.zundel@gmail.com>
2024-04-19 18:24:08 -0400

    added unordered map funcitonalities and changed main.cpp to use use insert

--- UnorderedMap.h
+++ UnorderedMap.h
@@ -15,14 +15,15 @@ using std::list;
 struct MovieInfo {
     string name;
     int year;
-    double rating;
-    MovieInfo(const string& name, int year, double rating)
-        : name(name), year(year), rating(rating) {}
+    MovieInfo(const string& name, int year)
+        : name(name), year(year) {} //wont do rating since file wont work
 };
 
 struct DirectorMovies {
     vector<MovieInfo> movies;
-    int movieCount;
+    size_t movieCount;//used size t to avoid errors since it is a type that is used in the standard library
+    //in addition, it is normally used to represent the size of an object
+    //https://pvs-studio.com/en/blog/terms/0044/#:~:text=size_t%20is%20a%20special%20unsigned,possible%20array%20or%20an%20object.
 
     void addMovie(const MovieInfo& movie) {
         movies.push_back(movie);
@@ -33,18 +34,81 @@ struct DirectorMovies {
 class UnorderedMap {
 private:
     struct KeyValuePair {
-        string key; //director's name
+        string key; //director's name (will be code)
         DirectorMovies value; //movies vector of struct that contains all infor for that movie
-        KeyValuePair(const std::string& key, const DirectorMovies& value)
+        KeyValuePair(const string& key, const DirectorMovies& value)
             : key(key), value(value) {}
     };
 
     vector<list<KeyValuePair>> buckets;
     hash<string> hasher;
     size_t bucketCount;
+    size_t size_;
 
+    void resizeAndRehash(size_t newCapacity) {
+        vector<list<KeyValuePair>> newBuckets(newCapacity);
+        for (auto& each_bucket : buckets) {
+            for (auto& each_item : each_bucket) {
+                size_t newBucketIndex = hasher(each_item.key) % newCapacity;
+                newBuckets[newBucketIndex].push_back(std::move(each_item));
+            }
+        }
+        buckets = std::move(newBuckets);
+        bucketCount = newCapacity;
+    }
     size_t getBucketIndex(const string& key) const {
-        return hasher(key) % bucketCount;
+        return hasher(key) % bucketCount; //from slack from TA's advice to reduce size of hash table
     }
 
+
 public:
+    UnorderedMap(size_t numBuckets)
+        : bucketCount(numBuckets), buckets(numBuckets), size_(0) {}
+
+    void insert(const string& directorID, const MovieInfo& movieInfo) {
+        if (size_ >= 0.75 * bucketCount) {
+            resizeAndRehash(bucketCount * 2); //from slides of final exam review discussion
+        }
+
+        size_t bucketIndex = getBucketIndex(directorID); 
+        auto& bucket = buckets[bucketIndex];
+        
+        for (auto& pair : bucket) {
+            if (pair.key == directorID) {
+                pair.value.addMovie(movieInfo);
+                return;
+            }
+        }
+        DirectorMovies directorMovies;
+        directorMovies.addMovie(movieInfo);
+        bucket.emplace_back(directorID, directorMovies);
+        ++size_; //increment size of hash table to calculate load factor
+    }
+
+    //below function literally code version of slide 54 from final exam review discussion
+    bool find(const string& directorName, DirectorMovies& directorMovies) const {
+        size_t bucketIndex = getBucketIndex(directorName);
+        const auto& bucket = buckets[bucketIndex];
+
+        for (const auto& element : bucket) {
+            if (element.key == directorName) {
+                directorMovies = element.value;
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    
+
+    size_t size() const {
+        size_t size = 0;
+        for (const auto& bucket : buckets) {
+            size += bucket.size();
+        }
+        return size;
+    }
+
+
+};
--- main.cpp
+++ main.cpp
@@ -28,13 +28,13 @@ void loadTitleBasics(Graph& graph, const string& filename, unordered_map<string,
         if (titleType == "movie") {
             graph.addNode(tconst, primaryTitle);
             int year = startYear != "\\N" ? std::stoi(startYear) : 0; //if N/A make it zero
-            movie_map[tconst] = {primaryTitle, year};; //will use map to first map the tconst to the primary title for both unordered map and max heap
+            movie_map[tconst] = {primaryTitle, year}; //will use map to first map the tconst to the primary title for both unordered map and max heap
         }
     }
     file.close();
 }
 
-void loadTitleCrew(Graph& graph, const string& filename, unordered_map<string, std::pair<string, int>>& movie_map) {
+void loadTitleCrew(Graph& graph, const string& filename, UnorderedMap& directorMap, unordered_map<string, std::pair<string, int>>& movie_map) {
     ifstream file(filename);
     if (!file.is_open()) {
         cerr << "Failed to open file: " + filename << endl;
@@ -49,7 +49,7 @@ void loadTitleCrew(Graph& graph, const string& filename, unordered_map<string, s
         getline(ss, tconst, '\t');
         getline(ss, directors, '\t');
         getline(ss, writers, '\t');
-
+        auto movieIt = movie_map.find(tconst);
         if (!directors.empty() && directors != "\\N") {
             string directorId;
             stringstream dirSS(directors);
@@ -60,9 +60,10 @@ void loadTitleCrew(Graph& graph, const string& filename, unordered_map<string, s
                     if (movieNode && directorNode) {
                         graph.addEdge(directorId, tconst); //might need to delete if we arent using graphs anymore
                     }
-                    if(movie_map.find(tconst) != movie_map.end()){
-                        
-                    }
+                if (!directorId.empty()) { //make sure we do not enter empty string
+                MovieInfo info(movieIt->second.first, movieIt->second.second);
+                directorMap.insert(directorId, info);
+                }
                 }
             }
         }
@@ -97,9 +98,10 @@ void loadTitleRatings(Graph& graph, const string& filename) {
 int main() {
      unordered_map<string, std::pair<string, int>> movieMap;
     Graph movieGraph;
+    UnorderedMap directorMap(100); //pick appropirate bucket starting size
     loadTitleBasics(movieGraph, "title-metadata.tsv", movieMap);
      std::cout << "Title: " << movieMap["tt0044895"].first << ", Year: " << movieMap["tt0044895"].second << std::endl; //for testing
-    loadTitleCrew(movieGraph, "title-crew.tsv", movieMap);
+    loadTitleCrew(movieGraph, "title-crew.tsv", directorMap, movieMap);
    //loadTitleRatings(movieGraph, "title.ratings.tsv"); doesnt work and wont need
     //movieGraph.display(); comment out for now for testing
     return 0;
